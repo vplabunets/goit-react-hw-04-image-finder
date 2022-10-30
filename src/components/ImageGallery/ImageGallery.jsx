bf8824f205a7438639fefae5b7ae4dd4dcbe101d
Volodymyr Labunets <vplabunets@gmail.com>
2022-10-30 11:14:14 +0200

    add env

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { apiPixabay } from 'api/apiPixabay';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
